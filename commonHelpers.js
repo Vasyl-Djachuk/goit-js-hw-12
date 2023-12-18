0659a15e7d884fb7b41cf1959b6c8948ce8defba
Vasyl-Djachuk <145469812+Vasyl-Djachuk@users.noreply.github.com>
2023-12-18 18:27:28 +0000

    Deploying to gh-pages from @ Vasyl-Djachuk/goit-js-hw-12@e7da846056cd467351ede5191749e9f04910eaf0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,24 +1,24 @@
-import{i as u,S as b}from"./assets/vendor-4971c7df.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();let o=0,d;const m=document.querySelector(".gallery"),c=document.querySelector(".loader");c.classList.remove("loader");const f=document.querySelector(".form");f.addEventListener("submit",v);document.querySelector(".page-section").insertAdjacentHTML("beforeend",'<button class="download-more">Download more</button>');const a=document.querySelector(".download-more");a.addEventListener("click",S);function v(r){r.preventDefault();let s=r.target.elements.search.value.trim();if(s===""){u.show({message:"❌ Field must be filled in",position:"topRight",color:"red"});return}f.reset(),m.innerHTML="",a.classList.remove("is-visibal"),c.classList.add("loader");const i=`https://pixabay.com/api/?${new URLSearchParams({key:"41274788-792c8d92905fcf9da75194117",q:`${s}`,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:100}).toString()}`;fetch(i).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>{if(e.hits.length===0){c.classList.remove("loader"),u.show({message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"}),a.classList.remove("is-visibal");return}w(e.hits)}).catch(e=>{console.log(e)})}function w(r){o=0,d=r;let s=r.slice(o,o+20);c.classList.remove("loader"),m.insertAdjacentHTML("afterbegin",p(s)),g.refresh(),r.length>=21&&a.classList.add("is-visibal")}function S(){o+=20,m.insertAdjacentHTML("beforeend",p(d.slice(o,o+20))),o+21>=d.length&&a.classList.remove("is-visibal"),g.refresh()}function p(r){let s="";return r.forEach(n=>{const{webformatURL:i,largeImageURL:e,tags:t,likes:l,views:h,comments:y,downloads:L}=n;s+=`<li class="gallery-item">
-          <a class="link" href="${e}">
-            <img class="form-img" src="${i}" alt="${t}" />
+import{a as v,S as w,i as u}from"./assets/vendor-eaddd480.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&l(n)}).observe(document,{childList:!0,subtree:!0});function a(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function l(e){if(e.ep)return;e.ep=!0;const r=a(e);fetch(e.href,r)}})();function h(s){let t="";return s.forEach(({webformatURL:a,largeImageURL:l,tags:e,likes:r,views:n,comments:L,downloads:b})=>{t+=`<li class="gallery-item">
+          <a class="link" href="${l}">
+            <img class="form-img" src="${a}" alt="${e}" />
             <ul class="description-list">
               <li class="description-item">
                 <p class="text-">Likes</p>
-                <p class="number">${l}</p>
+                <p class="number">${r}</p>
               </li>
               <li class="description-item">
                 <p class="text-">Views</p>
-                <p class="number">${h}</p>
+                <p class="number">${n}</p>
               </li>
               <li class="description-item">
                 <p class="text-">Comments</p>
-                <p class="number">${y}</p>
+                <p class="number">${L}</p>
               </li>
               <li class="description-item">
                 <p class="text-">Downloads</p>
-                <p class="number">${L}</p>
+                <p class="number">${b}</p>
               </li>
             </ul>
           </a>
-        </li>`}),s}const g=new b(".gallery .link",{captionsData:"alt",captionDelay:250});
+        </li>`}),t}async function g(s,t){const a=new URLSearchParams({key:"41274788-792c8d92905fcf9da75194117",q:`${s}`,image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:40,page:t}).toString();return(await v.get(`https://pixabay.com/api/?${a}`)).data}function S(){const s=window.innerHeight-100;window.scrollBy({top:s,behavior:"smooth"})}let o,d,m;const p=document.querySelector(".gallery"),i=document.querySelector(".loader");i.classList.remove("loader");const f=document.querySelector(".form"),y=new w(".gallery .link",{captionsData:"alt",captionDelay:250});document.querySelector(".page-section").insertAdjacentHTML("beforeend",'<button class="download-more">Load more</button>');const c=document.querySelector(".download-more");f.addEventListener("submit",async s=>{if(s.preventDefault(),d=s.target.elements.search.value.trim(),d===""){u.show({message:"❌ Field must be filled in",position:"topRight",color:"red"});return}i.classList.add("loader"),o=1,f.reset(),p.innerHTML="";try{const t=await g(d,o);if(t.hits.lenght===0){i.classList.remove("loader"),u.show({message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight",color:"red"}),c.classList.remove("is-visibal");return}i.classList.remove("loader"),p.insertAdjacentHTML("afterbegin",h(t.hits)),y.refresh(),m=Math.ceil(t.totalHits/40),o<m&&c.classList.add("is-visibal")}catch(t){console.log(t)}});c.addEventListener("click",async()=>{c.classList.remove("is-visibal"),i.classList.add("loader"),o++;try{const s=await g(d,o);if(i.classList.remove("loader"),p.insertAdjacentHTML("beforeend",h(s.hits)),y.refresh(),m===o){u.show({message:"We're sorry, but you've reached the end of search results.",position:"topRight",color:"blue"});return}c.classList.add("is-visibal"),S()}catch(s){console.log(s)}});
 //# sourceMappingURL=commonHelpers.js.map
